2e8040e3731046ec17fb71b966cec37873e215b1
parwaniayush20 <87536857+parwaniayush20@users.noreply.github.com>
2021-11-17 02:31:32 +0530

    Update app.js
    Hide the password on mongoose.connect.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
-mongoose.connect("mongodb+srv://admin-ayush:Test123@cluster0.qgfc6.mongodb.net/todolistDB",{useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-ayush:<PASSWORD>@cluster0.qgfc6.mongodb.net/todolistDB",{useNewUrlParser: true});
 
 const itemsSchema = {
     name: String,
